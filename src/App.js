ca2a058feaabf9ddfc23559c0f18e7dd79f25389
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-04-27 20:00:47 +0200

    Add tasks management

--- src/App.js
+++ src/App.js
@@ -5,32 +5,43 @@ import { Task } from "./components/Task";
 
 export function App() {
   const [user, setUser] = useState(null);
-  const [task, setTask] = useState(null);
+  const [tasks, setTasks] = useState([]);
 
   const handleLogin = (data) => setUser({ username: data.username });
+
   const handleAdd = (data) =>
-    setTask({ text: data.task, isDone: false });
-  const handleCheck = () =>
-    setTask((state) => ({
-      ...state,
-      isDone: !state.isDone,
-    }));
-  const handleDelete = () => setTask(null);
+    setTasks((state) => [...state, { text: data.task, isDone: false }]);
+
+  const handleCheck = (text) => {
+    setTasks((state) =>
+      state.map((task) => {
+        if (task.text === text) {
+          return { ...task, isDone: !task.isDone };
+        }
+
+        return task;
+      }),
+    );
+  };
+
+  const handleDelete = (text) =>
+    setTasks((state) => state.filter((task) => task.text !== text));
 
   return (
     <div>
       {user !== null && <div>Hi {user.username}!</div>}
       <LoginForm onLogin={handleLogin} />
       <TaskForm onAdd={handleAdd} />
-      {task !== null && (
+      {tasks.map((task) => (
         <Task
+          key={task.text}
           isDone={task.isDone}
-          onCheck={handleCheck}
-          onDelete={handleDelete}
+          onCheck={() => handleCheck(task.text)}
+          onDelete={() => handleDelete(task.text)}
         >
           {task.text}
         </Task>
-      )}
+      ))}
     </div>
   );
 }
