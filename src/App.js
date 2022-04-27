7ed68939572b0478bc078cfda9d9f0c2460f6043
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-04-27 18:48:44 +0200

    Add login form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+import { useState } from "react";
+import { LoginForm } from "./containers/LoginForm";
+
 export function App() {
-  return <div>Hello world!</div>;
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (data) => setUser({ username: data.username });
+
+  return (
+    <div>
+      {user !== null && <div>Hi {user.username}!</div>}
+      <LoginForm onLogin={handleLogin} />
+    </div>
+  );
 }
