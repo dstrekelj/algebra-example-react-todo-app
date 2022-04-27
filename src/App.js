b3556b8c7cf6d95af92f7dea728057ddfb29782b
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-04-27 19:27:20 +0200

    Add task display

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import { useState } from "react";
 import { LoginForm } from "./containers/LoginForm";
 import { TaskForm } from "./containers/TaskForm";
+import { Task } from "./components/Task";
 
 export function App() {
   const [user, setUser] = useState(null);
   const [task, setTask] = useState(null);
 
   const handleLogin = (data) => setUser({ username: data.username });
-  const handleAdd = (data) => setTask({ text: data.task });
+  const handleAdd = (data) =>
+    setTask({ text: data.task, isDone: false });
+  const handleCheck = () =>
+    setTask((state) => ({
+      ...state,
+      isDone: !state.isDone,
+    }));
+  const handleDelete = () => setTask(null);
 
   return (
     <div>
       {user !== null && <div>Hi {user.username}!</div>}
       <LoginForm onLogin={handleLogin} />
       <TaskForm onAdd={handleAdd} />
-      {task !== null && <div>{task.text}</div>}
+      {task !== null && (
+        <Task
+          isDone={task.isDone}
+          onCheck={handleCheck}
+          onDelete={handleDelete}
+        >
+          {task.text}
+        </Task>
+      )}
     </div>
   );
 }
