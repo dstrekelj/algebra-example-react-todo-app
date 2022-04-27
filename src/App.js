9e7966ed0682fb399375dbc12b8609c1960bed3a
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-04-27 19:07:01 +0200

    Add task form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { LoginForm } from "./containers/LoginForm";
+import { TaskForm } from "./containers/TaskForm";
 
 export function App() {
   const [user, setUser] = useState(null);
+  const [task, setTask] = useState(null);
 
   const handleLogin = (data) => setUser({ username: data.username });
+  const handleAdd = (data) => setTask({ text: data.task });
 
   return (
     <div>
       {user !== null && <div>Hi {user.username}!</div>}
       <LoginForm onLogin={handleLogin} />
+      <TaskForm onAdd={handleAdd} />
+      {task !== null && <div>{task.text}</div>}
     </div>
   );
 }
