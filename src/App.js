f666fd1a5a7f20faec9a3056e83690918ea07dae
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-04-27 20:28:02 +0200

    Add application context

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { LoginForm } from "./containers/LoginForm";
 import { TaskForm } from "./containers/TaskForm";
 import { Task } from "./components/Task";
+import { AppContext } from "./contexts/AppContext";
 
 export function App() {
-  const [user, setUser] = useState(null);
-  const [tasks, setTasks] = useState([]);
-
-  const handleLogin = (data) => setUser({ username: data.username });
-
-  const handleAdd = (data) =>
-    setTasks((state) => [
-      ...state,
-      { id: Date.now(), text: data.task, isDone: false },
-    ]);
-
-  const handleCheck = (id) => {
-    setTasks((state) =>
-      state.map((task) => {
-        if (task.id === id) {
-          return { ...task, isDone: !task.isDone };
-        }
-
-        return task;
-      }),
-    );
-  };
-
-  const handleDelete = (id) =>
-    setTasks((state) => state.filter((task) => task.id !== id));
-
-  let userInfo = null;
-  if (user !== null) userInfo = <div>Hi {user.username}!</div>;
-
-  console.log(tasks);
+  const appContext = useContext(AppContext);
 
   return (
     <div>
-      {userInfo}
-      <LoginForm onLogin={handleLogin} />
-      <TaskForm onAdd={handleAdd} />
-      {tasks.map((task) => (
+      <LoginForm onLogin={appContext.logIn} />
+      <TaskForm onAdd={appContext.addTask} />
+      {appContext.tasks.map((task) => (
         <Task
           key={task.id}
           isDone={task.isDone}
-          onCheck={() => handleCheck(task.id)}
-          onDelete={() => handleDelete(task.id)}
+          onCheck={() => appContext.checkTask(task.id)}
+          onDelete={() => appContext.deleteTask(task.id)}
         >
           {task.text}
         </Task>
